b16f3729e1dac608d0f7e16a89a04ad92a0ac33c
ozkayanurhuda <ozkayanurhuda@gmail.com>
2021-03-14 12:14:34 +0300

    with two dice

---
+++ app/src/main/java/com/nurozkaya/kotlindiceapp/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.nurozkaya.kotlindiceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val cleanButton:Button =findViewById(R.id.cleanButton)
+        cleanButton.setOnClickListener {
+            cleanDice()
+        }
+        val rollButton: Button =findViewById(R.id.rollDice)
+        rollButton.setOnClickListener {
+            Toast.makeText(this,"Dice Rolled!", Toast.LENGTH_SHORT).show()
+            rollDice()
+            rollDice2()
+        }
+        cleanDice()
+    }
+    private fun cleanDice() {
+        val diceImage: ImageView =findViewById(R.id.imageView)
+        val diceImage2: ImageView =findViewById(R.id.imageView2)
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+        Toast.makeText(this,"Cleared", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun rollDice() {
+        val dice=Dice(6)
+        val diceRoll=dice.roll()
+        val diceImage: ImageView =findViewById(R.id.imageView)
+        val drawableResource=when(diceRoll) {
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else-> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription=diceRoll.toString()
+    }
+    private fun rollDice2() {
+        val dice=Dice(6)
+        val diceRoll=dice.roll()
+        val diceImage: ImageView =findViewById(R.id.imageView2)
+        val drawableResource=when(diceRoll) {
+            1-> R.drawable.dice_1
+            2-> R.drawable.dice_2
+            3-> R.drawable.dice_3
+            4-> R.drawable.dice_4
+            5-> R.drawable.dice_5
+            else-> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription=diceRoll.toString()
+    }
+}
+class Dice(private val numSides: Int) {
+    fun roll():Int {
+        return (1..numSides).random()
+    }
+
+}
